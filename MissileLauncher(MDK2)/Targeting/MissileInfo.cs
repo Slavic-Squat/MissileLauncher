8e84b0d16ce9612f91168a5761a4b8771d0b4e5c
Slavic-Squat <jahea98@gmail.com>
2025-10-09 14:46:19 -0500

    -Forgot to change MissileInfo

--- MissileLauncher(MDK2)/Targeting/MissileInfo.cs
+++ MissileLauncher(MDK2)/Targeting/MissileInfo.cs
@@ -28,12 +28,13 @@ namespace IngameScript
             public long LauncherID { get; private set; }
             public MissileStage Stage { get; private set; }
             public MissileType Type { get; private set; }
+            public MissileGuidanceType Guidance { get; private set; }
             public MissilePayload Payload { get; private set; }
             public long TargetID { get; private set; }
             public long Address { get; private set; }
             public bool IsValid { get; private set; }
 
-            public MissileInfo(long launcherID, long targetID, MissileStage stage, MissileType type, MissilePayload payload, long address)
+            public MissileInfo(long launcherID, long targetID, MissileStage stage, MissileType type, MissileGuidanceType guidance, MissilePayload payload, long address)
             {
                 LauncherID = launcherID;
                 TargetID = targetID;
@@ -41,6 +42,7 @@ namespace IngameScript
                 Type = type;
                 Payload = payload;
                 Address = address;
+                Guidance = guidance;
                 IsValid = true;
             }
 
@@ -51,6 +53,7 @@ namespace IngameScript
                 bytes.AddRange(BitConverter.GetBytes(LauncherID));
                 bytes.Add((byte)Stage);
                 bytes.Add((byte)Type);
+                bytes.Add((byte)Guidance);
                 bytes.Add((byte)Payload);
                 bytes.AddRange(BitConverter.GetBytes(TargetID));
                 bytes.AddRange(BitConverter.GetBytes(Address));
@@ -66,13 +69,15 @@ namespace IngameScript
                 index += 1;
                 MissileType type = (MissileType)data[index];
                 index += 1;
+                MissileGuidanceType guidance = (MissileGuidanceType)data[index];
+                index += 1;
                 MissilePayload payload = (MissilePayload)data[index];
                 index += 1;
                 long targetID = BitConverter.ToInt64(data, index);
                 index += 8;
                 long address = BitConverter.ToInt64(data, index);
                 index += 8;
-                return new MissileInfo(launcherID, targetID, stage, type, payload, address);
+                return new MissileInfo(launcherID, targetID, stage, type, guidance, payload, address);
             }
         }
     }
