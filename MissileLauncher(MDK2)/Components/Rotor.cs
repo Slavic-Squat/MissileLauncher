831ec6af5f4dfa43c5b73075f936ac21fb0594ac
Johnny Hea <jahea98@gmail.com>
2025-11-02 00:42:53 -0700

    -Changed Rotor

--- MissileLauncher(MDK2)/Components/Rotor.cs
+++ MissileLauncher(MDK2)/Components/Rotor.cs
@@ -92,17 +92,19 @@ namespace IngameScript
                 }
             }
 
-            public bool IsMaxedOut
+            public bool IsMaxed => CurrentAngle >= MaxAngle - MathHelper.EPSILON;
+            public bool IsMinned => CurrentAngle <= MinAngle + MathHelper.EPSILON;
+            public bool IsSaturated
             {
                 get
                 {
                     if (Velocity > 0)
                     {
-                        return CurrentAngle >= MaxAngle - MathHelper.EPSILON;
+                        return IsMaxed;
                     }
                     else if (Velocity < 0)
                     {
-                        return CurrentAngle <= MinAngle + MathHelper.EPSILON;
+                        return IsMinned;
                     }
                     else
                     {
